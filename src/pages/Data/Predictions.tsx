3178e592970303cc014f6e2046081a1ce6d4247c
Sebastian Lopez <sebas320lopez@hotmail.com>
2026-04-30 21:06:58 -0500

    e modifica para que se realice la peticion cada vez que se seleccione un producto

--- src/pages/Data/Predictions.tsx
+++ src/pages/Data/Predictions.tsx
@@ -4,16 +4,19 @@ import ProductSelecter from "@/components/ui/ProductSelecter"
 export default function Predictions() {
     const [product, setProduct] = useState("")
     useEffect(() => {
+        if (!product) return; // evita llamada vacía
+
         const fetchData = async () => {
-            const response = await getPrediction(product)//esto viene del input
+            const response = await getPrediction(product)
             console.log(response)
         }
+
         fetchData()
-    },[]) 
+    }, [product])
 
     return (<>
-    <h1>my prediction page</h1>
-    <ProductSelecter onSelect={(value) => setProduct(value)} />
-    <h1>{product}</h1>
+        <h1>my prediction page</h1>
+        <ProductSelecter onSelect={(value) => setProduct(value)} />
+        <h1>{product}</h1>
     </>)
 }
