03bedbd2130b5a1ee313c03ba5c825cd8d1bd795
darckronoz <crispipe14@gmail.com>
2026-05-05 09:11:03 -0500

    integrar cambios de retorno ventas filtradas

--- src/utils/files/DataFilter.ts
+++ src/utils/files/DataFilter.ts
@@ -1,5 +1,6 @@
 import type { SaleRow } from "@/types/SalesTypes";
 import { getSalesTableDataByFilters } from "@/services/sales";
+import type { dateFilterData } from "@/types/filtersTypes";
 
 
 //Las funciones de utils no deberian hacer llamados al back,
@@ -47,3 +48,45 @@ export const getRowsForTable = async (
         return [];
     }
 };
+
+const formatDate = (date: Date) => {
+        return date.toISOString().split('.')[0];
+};
+
+export function getDateRange(data: dateFilterData): [string | null, string | null] {
+    const { year, month, day } = data;
+
+    let from: string | null = null;
+    let to: string | null = null;
+
+    if (year) {
+        const y = parseInt(year);
+
+        if (month) {
+            const m = parseInt(month) - 1;
+
+            if (day) {
+                const d = parseInt(day);
+
+                const date = new Date(y, m, d);
+                from = formatDate(date);
+                to = formatDate(date);
+
+            } else {
+                const start = new Date(y, m, 1);
+                const end = new Date(y, m + 1, 0);
+
+                from = formatDate(start);
+                to = formatDate(end);
+            }
+
+        } else {
+            const start = new Date(y, 0, 1);
+            const end = new Date(y, 11, 31);
+
+            from = formatDate(start);
+            to = formatDate(end);
+        }
+    }
+    return [from, to]
+}
